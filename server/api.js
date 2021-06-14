0a358f94e38aa3d3f5d14acc5cc66d641b389781
myerkes <mattyerkes23@gmail.com>
2021-06-14 17:46:07 -0400

    Complete Minions and Idea router functions

--- server/api.js
+++ server/api.js
@@ -28,6 +28,16 @@ deleteAllFromDatabase(modelName)
 const minionRouter = express.Router({mergeParams: true});
 apiRouter.use('/minions', minionRouter);
 
+minionRouter.param('minionId', (req, res, next, id) => {
+    const minion = db.getFromDatabaseById('minions', id);
+    if (minion) {
+      req.minion = minion;
+      next();
+    } else {
+      res.status(404).send();
+    }
+});
+
 // GET all
 minionRouter.get('/', (req, res, next) => {
     let minions = db.getAllFromDatabase('minions');
@@ -42,27 +52,57 @@ minionRouter.get('/', (req, res, next) => {
 
 // GET one
 minionRouter.get('/:minionId', (req, res, next) => {
-
+    res.status(200).send(req.minion);
 });
 
 // POST create new minion and save to database
 minionRouter.post('/', (req, res, next) => {
-
+    try {
+        let minion = req.body;
+        db.addToDatabase('minions', minion);
+        res.status(201).send(minion);
+        next();
+    } catch(err) {
+        res.status(400).send(err.message);
+    }
 });
 
 // PUT update minion by id
 minionRouter.put('/:minionId', (req, res, next) => {
-
+    try {
+        let updMinion = db.updateInstanceInDatabase('minions', req.body);
+        if (updMinion) {
+            res.status(201).send(updMinion);
+            next();
+        }
+        else {
+            throw new Error('invalid inputs');
+        }
+    } catch(err) {
+        res.status(400).send(err.message);
+    }
 });
 
 // DELETE minion by id
 minionRouter.delete('/:minionId', (req, res, next) => {
-    
+    db.deleteFromDatabasebyId('minions', req.params.minionId);
+    res.status(204).send();
+    next();
 });
 
 ///// Ideas /////
 const ideaRouter = express.Router({mergeParams: true});
 apiRouter.use('/ideas', ideaRouter);
+
+ideaRouter.param('ideaId', (req, res, next, id) => {
+    const idea = db.getFromDatabaseById('ideas', id);
+    if (idea) {
+      req.idea = idea;
+      next();
+    } else {
+      res.status(404).send();
+    }
+});
 
 // GET all
 ideaRouter.get('/', (req, res, next) => {
@@ -78,22 +118,42 @@ ideaRouter.get('/', (req, res, next) => {
 
 // GET one
 ideaRouter.get('/:ideaId', (req, res, next) => {
-
+    res.status(200).send(req.idea);
 });
 
 // POST create new idea and save to database
 ideaRouter.post('/', (req, res, next) => {
-
+    try {
+        let idea = req.body;
+        db.addToDatabase('ideas', idea);
+        res.status(201).send(idea);
+        next();
+    } catch(err) {
+        res.status(400).send(err.message);
+    }
 });
 
 // PUT update idea by id
 ideaRouter.put('/:ideaId', (req, res, next) => {
-
+    try {
+        let updIdea = db.updateInstanceInDatabase('ideas', req.body);
+        if (updIdea) {
+            res.status(201).send(updIdea);
+            next();
+        }
+        else {
+            throw new Error('invalid inputs');
+        }
+    } catch(err) {
+        res.status(400).send(err.message);
+    }
 });
 
 // DELETE idea by id
 ideaRouter.delete('/:ideaId', (req, res, next) => {
-
+    db.deleteFromDatabasebyId('ideas', req.params.ideaId);
+    res.status(204).send();
+    next();
 });
 
 ///// Meetings /////
@@ -113,14 +173,28 @@ meetingRouter.get('/', (req, res, next) => {
 });
 
 // POST create new meeting and save to database
-meetingRouter.post('/meetings', (req, res, next) => {
-    
+meetingRouter.post('/', (req, res, next) => {
+    try {
+        let meeting = req.body;
+        db.addToDatabase('meetings', meeting);
+        res.status(201).send(meeting);
+        console.log('working');
+        next();
+    } catch(err) {
+        res.status(400).send(err.message);
+    }
 });
 
 // DELETE all meetings
-meetingRouter.delete('/meetings', (req, res, next) => {
-    db.deleteAllFromDatabase('meetings');
-    next();
+meetingRouter.delete('/', (req, res, next) => {
+   let newSchedule = db.deleteAllFromDatabase('meetings');
+   if (newSchedule === []) {
+       res.status(204).send();
+       next();
+   }
+   else {
+       res.status(400).send();
+   }
 });
 
 
